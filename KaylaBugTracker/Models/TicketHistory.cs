3110c378ef72118fd933632181ce745f4a98a490
KMcGraw806 <67701155+KMcGraw806@users.noreply.github.com>
2020-08-04 13:10:25 -0400

    Additions
    Added Models, Controllers and Helpers

---
+++ KaylaBugTracker/Models/TicketHistory.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KaylaBugTracker.Models
+{
+    public class TicketHistory
+    {
+        public int Id { get; set; }
+
+        #region Parents/Children
+        public int TicketId { get; set; }
+        public virtual Ticket Ticket { get; set; }
+
+        public string UserId { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        #endregion
+
+        #region Actual Properties
+        //The property of the ticket that was changed (Status, Type, Attachment added etc.)
+        public string Property { get; set; }
+
+        //What the property was originally set to
+        public string OldValue { get; set; }
+
+        //What the property is now set to
+        public string NewValue { get; set; }
+
+        public DateTime ChangedOn { get; set; }
+        #endregion
+    }
+}
